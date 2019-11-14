493a9ec73725a4eb0157e360f3243a323597ee48
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-11-14 11:13:52 -0700

    Model Products

--- UKartApp/app/src/main/java/com/example/ukartapp/Models/Products.java
+++ UKartApp/app/src/main/java/com/example/ukartapp/Models/Products.java
@@ -5,13 +5,50 @@ public class Products {
     private String id;
     private String name;
     private String description;
+    private String price;
+    private String qr;
+    private String icon;
 
     public Products(){}
 
-    public Products(String id, String name, String description){
+    public Products(String id, String name, String description, String price, String qr, String icon){
         this.id = id;
         this.name = name;
         this.description = description;
+        this.price = price;
+        this.qr = qr;
+    }
+
+    public Products(String name, String description, String price, String qr, String icon){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.qr = qr;
+        this.icon = icon;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQr() {
+        return qr;
+    }
+
+    public void setQr(String qr) {
+        this.qr = qr;
     }
 
     public String getId() {
